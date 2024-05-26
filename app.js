0dca83c6f2b23922505d54176450d8330e3e9550
Darkzero7 <chaiyanituporn@gmail.com>
2024-05-26 23:27:26 +0700

    js lab 74

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ function fixNum(num) {
     return num.toFixed(2);
   } else {
     let number = parseFloat(txt.slice(0, txt.indexOf(".") + 3));
-    return number.toFixed(2);
+    return number
   }
 }
 
@@ -13,3 +13,4 @@ console.log(fixNum(10));
 console.log(fixNum(5.1));
 console.log(fixNum(3.1289));
 console.log(fixNum(7.88888));
+console.log(fixNum(1234587.8833223888));
